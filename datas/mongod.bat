edc3e5ef7ea3ee2e572b8a9ef18daf496dac237a
Truedick23 <dingzhx@vip.qq.com>
2019-04-14 20:00:31 +0800

    bat added

---
+++ datas/mongod.bat
@@ -0,0 +1,5 @@
+mongoimport --db felkub --collection directors --file directors.json
+mongoimport --db felkub --collection movies --file movies.json
+mongoimport --db felkub --collection starring --file starring.json
+mongoimport --db felkub --collection tags --file tags.json
+mongoimport --db felkub --collection genres --file genres.json
